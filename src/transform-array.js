90ea700ba5dcc4f973a33a928e3985113f72156c
yanabel1996 <119679434+yanabel1996@users.noreply.github.com>
2023-04-17 20:49:37 +0200

    feat: fix error

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,19 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  const array = arr;
-  if (typeof array === 'array') throw new Error('\'arr\' parameter must be an instance of the Array!');
-  if (array.includes('--discard-next')) {
-    return array.splice(array[array.indexOf('--discard-next') + 1], 1);
-  } else if (array.includes('--discard-prev')) {
-    return array.splice(array[array.indexOf('--discard-prev') - 1], 1);
-  } else if (array.includes('--double-next')) {
-    const elem = array.slice(array[array.indexOf('--discard-next') + 1], -1)
-    return array.splice(array[array.indexOf('--discard-next') + 1], 0, elem);
-  } else if (array.includes('--double-prev')) {
-    const elem = array.slice(array[array.indexOf('--discard-prev') - 1], -1)
-    return array.splice(array[array.indexOf('--discard-prev') - 1], 0, elem);
+
+  const chengedArray = [];
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
   }
+  const array = arr.slice();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
+        chengedArray.pop();
+      }
+    }
+    else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+        chengedArray.push(arr[i + 1]);
+      }
+    }
+    else if (arr[i] === '--double-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
+        chengedArray.push(arr[i - 1]);
+      }
+    }
+    else {
+      chengedArray.push(arr[i]);
+    }
+  }
+  return chengedArray;
 }
 
 module.exports = {
